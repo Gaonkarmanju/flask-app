a78f56299058d0ada71282848f207f63235c5b2c
Gaonkarmanju <manjunathgaonkar300@gmail.com>
2025-07-18 00:05:07 +0530

    Added port

--- app.py
+++ app.py
@@ -160,4 +160,7 @@ if __name__ == '__main__':
     if not os.path.exists('todo.db'):
         with app.app_context():
             db.create_all()
-    app.run(debug=True)
+
+    import os
+    port = int(os.environ.get('PORT', 5000))  # fallback to 5000 locally
+    app.run(host='0.0.0.0', port=port)
